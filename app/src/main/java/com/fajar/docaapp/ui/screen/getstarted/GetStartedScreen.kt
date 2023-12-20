183eccdaa61511217d1eefc88b98904b71b0b8ce
Ernest Widjaja <ernestwidjaja0@gmail.com>
2023-12-20 13:54:30 +0700

    adding image function

--- app/src/main/java/com/fajar/docaapp/ui/screen/getstarted/GetStartedScreen.kt
+++ app/src/main/java/com/fajar/docaapp/ui/screen/getstarted/GetStartedScreen.kt
@@ -1,24 +1,24 @@
 package com.fajar.docaapp.ui.screen.getstarted
 
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -28,12 +28,21 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import coil.compose.AsyncImage
 import com.fajar.docaapp.R
 import com.fajar.docaapp.ui.components.ContainerBox
 import com.fajar.docaapp.ui.components.ContainerPage
 import com.fajar.docaapp.ui.components.OutlinedButtonWithIcon
 import com.fajar.docaapp.ui.navigation.Screen
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.FileProvider
+import coil.compose.AsyncImage
+import com.fajar.docaapp.BuildConfig
+import com.fajar.docaapp.utils.getImageUri
+import java.util.Objects
+import androidx.compose.runtime.remember as remember
 
 @Composable
 fun GetStartedScreen(
@@ -78,10 +87,29 @@ fun GetStartedScreen(
 fun GetStartedContent(
     modifier: Modifier = Modifier
 ) {
-    Image(
-        painter = painterResource(id = R.drawable.ic_place_holder),
+    val context = LocalContext.current
+    val file = getImageUri(context)
+    var selectedImageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val photoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia()
+    ) {
+        selectedImageUri = it
+    }
+    val cameraLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.TakePicture()
+    ) {
+        if (it) {
+            selectedImageUri = file
+        }
+    }
+
+    AsyncImage(
+        model = selectedImageUri,
         contentDescription = null,
         contentScale = ContentScale.Crop,
+        error = painterResource(R.drawable.ic_place_holder),
         modifier = Modifier
             .size(400.dp)
             .clip(RoundedCornerShape(30.dp))
@@ -93,11 +121,19 @@ fun GetStartedContent(
     ) {
         OutlinedButtonWithIcon(
             text = "Gallery",
-            icon = R.drawable.baseline_folder_24
+            icon = R.drawable.baseline_folder_24,
+            onClick = {
+                photoPickerLauncher.launch(
+                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                )
+            }
         )
         OutlinedButtonWithIcon(
             text = "Camera",
-            icon = R.drawable.baseline_camera_alt_24
+            icon = R.drawable.baseline_camera_alt_24,
+            onClick = {
+                cameraLauncher.launch(file)
+            }
         )
     }
     Spacer(modifier = Modifier.height(4.dp))
